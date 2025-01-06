91f5b0197abb549f0cb71df7eb28999eca2a002d
Santhosh1l <e.g.santhosh2004@gmail.com>
2025-01-06 15:51:48 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	fibonacci_series.java
    #	practice.java
    #	remove_duplicates.java
    #	reverse_string.java
    #	timeconvert.java

--- reverse_string.java
+++ reverse_string.java
@@ -1,6 +1,12 @@
 public class reverse_string {
     public static void main(String[] args) {
         String str = "Hello world";
+      String  ch[]=str.split(" ");
+        for(int i=ch.length-1;i>=0;i--){
+            System.out.print(ch[i]);
+        if(i>0){
+            System.out.print(" ");
+        }}
 
             }
         }
